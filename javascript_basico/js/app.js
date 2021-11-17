3623883955a301987ab057b6ba9b8c4eb5b61621
LaureanoVera <laureanoigvera@gmail.com>
2021-11-17 15:09:44 -0300

    curso practico de javascript: interactuando con js desde html

--- javascript_basico/js/app.js
+++ javascript_basico/js/app.js
@@ -4,17 +4,16 @@ console.group("Cuadrado");
 const perimetroCuadrado = (lado) => {
   let res = lado * 4;
   console.log(`El perimetro del cuadrado es: ${res}cm`);
-  return res
-}
-perimetroCuadrado(4)
-
+  return res;
+};
+perimetroCuadrado(4);
 
 const areaCuadrado = (lado) => {
-  let res = lado * lado
+  let res = lado * lado;
   console.log(`El area del cuadrado es: ${res}cm^2`);
-  return res
-}
-areaCuadrado(4)
+  return res;
+};
+areaCuadrado(4);
 
 console.groupEnd();
 
@@ -26,46 +25,60 @@ const ladoTriangulo2 = 6;
 const baseTriangulo = 4;
 const alturaTriangulo = 5.5;
 
-const perimetroTriangulo = ( lado1, lado2, base) => {
-  let res = lado1 + lado2 + base
+const perimetroTriangulo = (lado1, lado2, base) => {
+  let res = lado1 + lado2 + base;
   console.log(`El perimetro del cuadrado es: ${res}`);
-  return res
-}
-perimetroTriangulo(ladoTriangulo1, ladoTriangulo2, baseTriangulo)
+  return res;
+};
+perimetroTriangulo(ladoTriangulo1, ladoTriangulo2, baseTriangulo);
 
 const areaTriangulo = (base, alto) => {
-  let res = (baseTriangulo * alturaTriangulo) / 2
+  let res = (baseTriangulo * alturaTriangulo) / 2;
   console.log(`El areadel triangulo es: ${res}cm^2`);
-  return res
+  return res;
 };
-areaTriangulo(baseTriangulo, alturaTriangulo)
+areaTriangulo(baseTriangulo, alturaTriangulo);
 
 console.groupEnd();
 
 // CIRCULO
 console.group("Circulo");
 
-const PI = Math.PI
+const PI = Math.PI;
 
 const diametroCirculo = (radio) => {
-  let res = radio * 2
+  let res = radio * 2;
   console.log(`El diametro del circulo es: ${res}cm`);
-  return res
-}
+  return res;
+};
 
 const perimetroCirculo = (radio) => {
-  const diametro = diametroCirculo(radio)
-  let res = diametro * PI
+  const diametro = diametroCirculo(radio);
+  let res = diametro * PI;
   console.log(`El perimetro del circulo es: ${res.toFixed(2)}cm`);
-  return res
-}
-perimetroCirculo(4)
+  return res;
+};
+perimetroCirculo(4);
 
 const areaCirculo = (radio) => {
-  let res = (radio * radio) * PI
+  let res = radio * radio * PI;
   console.log(`El area del circulo es: ${res.toFixed(2)}cm`);
-  return res
-}
-areaCirculo(4)
+  return res;
+};
+areaCirculo(4);
 
 console.groupEnd();
+
+// INTERACCION CON DOM
+const inputCuadrado = document.getElementById("inputCuadrado")
+const calcularPerimetroCuadrado = () => {
+  const value = inputCuadrado.value
+  const perimetro = perimetroCuadrado(value)
+  alert(perimetro)
+}
+
+const calcularAreaCuadrado = () => {
+  const value = inputCuadrado.value
+  const area = areaCuadrado(value)
+  alert(area)
+}
