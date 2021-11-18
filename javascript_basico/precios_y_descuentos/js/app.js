245b2549188748a6d7122806493866de8e8a0246
LaureanoVera <laureanoigvera@gmail.com>
2021-11-18 16:07:55 -0300

    curso practico javascript: escribiendo html desde javascript

--- javascript_basico/precios_y_descuentos/js/app.js
+++ javascript_basico/precios_y_descuentos/js/app.js
@@ -1,8 +1,20 @@
 const precioOriginal = 200;
 const descuento = 15;
+
 const calcularConDescuento = (precio, desc) => {
-  return precio * ((100 - desc) / 100)
+  return precio * ((100 - desc) / 100);
+};
+
+const mostrarResultado = (precio) => {
+  const elementoParrafo = document.getElementById('resultPrice')
+  const resultadoTexto = `Precio final con descuento es: $${precio}`
+  elementoParrafo.innerText = resultadoTexto
 }
 
-const precioFinal = calcularConDescuento(precioOriginal, descuento)
-console.log(precioFinal);
+const handlePriceDiscount = () => {
+  const price = document.getElementById("price").value;
+  const discount = document.getElementById("discount").value;
+  const precioFinal = calcularConDescuento(price, discount);
+  mostrarResultado(precioFinal)
+};
+
