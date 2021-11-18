aff9d496701ea86598a6646764cec58f032916ea
LaureanoVera <laureanoigvera@gmail.com>
2021-11-18 09:46:11 -0300

    curso practico javascript: crea una pagina web para calcular descuentos

--- javascript_basico/precios_y_descuentos/js/app.js
+++ javascript_basico/precios_y_descuentos/js/app.js
@@ -1,1 +1,8 @@
-console.log('OK');
+const precioOriginal = 200;
+const descuento = 15;
+const calcularConDescuento = (precio, desc) => {
+  return precio * ((100 - desc) / 100)
+}
+
+const precioFinal = calcularConDescuento(precioOriginal, descuento)
+console.log(precioFinal);
