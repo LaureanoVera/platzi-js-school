d83d547cc401e8c7f325e01f92563f3d9016f0b7
LaureanoVera <laureanoigvera@gmail.com>
2021-11-18 09:32:39 -0300

    curso practico javascript: organizacion de proyectos

--- javascript_basico/figuras_geometricas/js/app.js
+++ javascript_basico/figuras_geometricas/js/app.js
@@ -1,73 +1,48 @@
 // CUADRADO
-console.group("Cuadrado");
-
 const perimetroCuadrado = (lado) => {
-  let res = lado * 4;
-  console.log(`El perimetro del cuadrado es: ${res}cm`);
-  return res;
+  return res = lado * 4;
 };
 perimetroCuadrado(4);
 
 const areaCuadrado = (lado) => {
-  let res = lado * lado;
-  console.log(`El area del cuadrado es: ${res}cm^2`);
-  return res;
+  return res = lado * lado;
 };
 areaCuadrado(4);
 
-console.groupEnd();
-
 // TRIANGULO
-console.group("Triangulo");
-
 const ladoTriangulo1 = 6;
 const ladoTriangulo2 = 6;
 const baseTriangulo = 4;
 const alturaTriangulo = 5.5;
 
 const perimetroTriangulo = (lado1, lado2, base) => {
-  let res = lado1 + lado2 + base;
-  console.log(`El perimetro del cuadrado es: ${res}`);
-  return res;
+  return res = lado1 + lado2 + base;
 };
 perimetroTriangulo(ladoTriangulo1, ladoTriangulo2, baseTriangulo);
 
 const areaTriangulo = (base, alto) => {
-  let res = (baseTriangulo * alturaTriangulo) / 2;
-  console.log(`El areadel triangulo es: ${res}cm^2`);
-  return res;
+  return res = (baseTriangulo * alturaTriangulo) / 2;
 };
 areaTriangulo(baseTriangulo, alturaTriangulo);
 
-console.groupEnd();
-
 // CIRCULO
-console.group("Circulo");
-
 const PI = Math.PI;
 
 const diametroCirculo = (radio) => {
-  let res = radio * 2;
-  console.log(`El diametro del circulo es: ${res}cm`);
-  return res;
+  return res = radio * 2;
 };
 
 const perimetroCirculo = (radio) => {
   const diametro = diametroCirculo(radio);
-  let res = diametro * PI;
-  console.log(`El perimetro del circulo es: ${res.toFixed(2)}cm`);
-  return res;
+  return res = diametro * PI;
 };
 perimetroCirculo(4);
 
 const areaCirculo = (radio) => {
-  let res = radio * radio * PI;
-  console.log(`El area del circulo es: ${res.toFixed(2)}cm`);
-  return res;
+  return res = radio * radio * PI;
 };
 areaCirculo(4);
 
-console.groupEnd();
 
 // INTERACCION CON DOM
 const inputCuadrado = document.getElementById("inputCuadrado")
