53af380a58822e3e7e8afe22cddbf8a96301d0d2
LaureanoVera <laureanoigvera@gmail.com>
2021-11-18 19:17:29 -0300

    curso de ECMAScript 6+: promesas

--- ecmascript/src/es6/index.js
+++ ecmascript/src/es6/index.js
@@ -64,30 +64,44 @@ const a = "a";
 // a = "b"; <== ERROR
 
 // PARAMETRPOS EN OBJETOS
-let user = 'User00202'
-let pass = 123123123123123
-let obj = {user: user, pass:pass}
+let user = "User00202";
+let pass = 123123123123123;
+let obj = { user: user, pass: pass };
 console.log(obj);
-let obj2 = {user, pass}
+let obj2 = { user, pass };
 console.log(obj2);
 
 const names = [
-  {name: 'Laureano', age: 18},
-  {name: 'Ivan', age: 19},
-  {name: 'Gerardo', age: 20}
-]
+  { name: "Laureano", age: 18 },
+  { name: "Ivan", age: 19 },
+  { name: "Gerardo", age: 20 },
+];
 
 // ARROW FUNCTIONS
-let listOfNames = names.map(function(el) {
+let listOfNames = names.map(function (el) {
   return el.name;
-})
+});
 console.log(listOfNames);
 
-let listOfNames2 = names.map(el => el.name)
+let listOfNames2 = names.map((el) => el.name);
 console.log(listOfNames2);
 
 const listOfNames3 = (name) => {
   console.log(name);
-}
+};
 
-const listOfNames3 = name => console.log(name);
+const listOfNames3 = (name) => console.log(name);
+
+// PROMESAS
+const helloPromise = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve("Hey!");
+    } else {
+      reject("Ups...");
+    }
+  });
+};
+helloPromise()
+  .then((response) => console.log(response))
+  .catch((error) => console.log(error));
