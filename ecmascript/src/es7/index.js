8b6cf94daf3a3a8b8ccc356807fd625184693dd6
LaureanoVera <laureanoigvera@gmail.com>
2021-11-20 14:33:15 -0300

    curso de ECMAScript 6+: que se implemento en es7

---
+++ ecmascript/src/es7/index.js
@@ -0,0 +1,14 @@
+// INCLUDES
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+if (numbers.includes(5)) {
+  console.log("Si se encuentra el valor 5");
+} else {
+  console.log("No se encuentra el valor 5");
+}
+
+// POTENCIA
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+console.log(result);
