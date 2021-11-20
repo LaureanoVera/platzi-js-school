350bb3065177aa95c1a0de943669d0bf87736a0a
LaureanoVera <laureanoigvera@gmail.com>
2021-11-20 15:20:06 -0300

    curso de ECMAScript 6+: async await

--- ecmascript/src/es8/index.js
+++ ecmascript/src/es8/index.js
@@ -23,3 +23,30 @@ console.log(values.length);
 const string = 'hello'
 console.log(string.padStart(7, 'hi'));
 console.log(string.padEnd(12, '...'));
+
+// ASYNC AWAIT
+const helloPromise = () => {
+  return new Promise((resolve, reject) => {
+    (true)
+      ? setTimeout(() => {
+        resolve('Hello World')
+      }, 3000)
+      : reject(new Error('Test Error'))
+  })
+}
+
+const helloAsync = async () => {
+  const hello = await helloPromise()
+  console.log(hello);
+}
+helloAsync()
+
+const anotherFuction = async () => {
+  try {
+    const hello = await helloPromise()
+    console.log(hello);
+  } catch(error) {
+    console.log(error);
+  }
+}
+anotherFuction()
