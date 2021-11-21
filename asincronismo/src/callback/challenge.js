98e414ddb090f5fb39ae0acd8fe868bdc9d9efce
LaureanoVera <laureanoigvera@gmail.com>
2021-11-21 11:12:00 -0300

    curso de asincronismo con javascript: peticiones a APIs usando callbacks

--- asincronismo/src/callback/challenge.js
+++ asincronismo/src/callback/challenge.js
@@ -1,5 +1,9 @@
 let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
+// API rick and morty
+const API = 'https://rickandmortyapi.com/api/character/'
+
+// main
 function fetchData(urlApi, callback) {
   let xhttp = new XMLHttpRequest()
   xhttp.open('GET', urlApi, true)
@@ -15,3 +19,16 @@ function fetchData(urlApi, callback) {
   }
   xhttp.send()
 }
+
+fetchData(API, function(error1, data1) {
+  if (error1) return console.error(error1);
+  fetchData(API + data1.results[0].id, function(error2, data2) {
+    if(error2) return console.error(erro2);
+    fetchData(data2.origin.url, function(error3, data3) {
+      if (error3) return console.error(error3);
+      console.log(data1.info.count);
+      console.log(data2.name);
+      console.log(data3.dimension);
+    })
+  })
+})
