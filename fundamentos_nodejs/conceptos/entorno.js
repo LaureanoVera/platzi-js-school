99aa92105507b9fcfff24f2b3dfbd563cc2e08a1
LaureanoVera <laureanoigvera@gmail.com>
2021-12-25 20:07:52 -0300

    curso de fundamentos de nodejs: monohilo implicaciones en diseño y seguridad

---
+++ fundamentos_nodejs/conceptos/entorno.js
@@ -0,0 +1,2 @@
+let name = process.env.NAME
+console.log(name);
---
+++ fundamentos_nodejs/conceptos/monohilo.js
@@ -0,0 +1,8 @@
+console.log('Hello World');
+
+let i = 0
+setInterval(function() {
+  console.log(i);
+  i++
+  console.log("I'm still active");
+}, 2000)
