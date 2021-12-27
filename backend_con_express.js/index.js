25a2406c895772f9d781e5966f78956427c957e8
LaureanoVera <laureanoigvera@gmail.com>
2021-12-27 18:06:20 -0300

    backend con nodejs: api con express: routing con express

--- backend_con_express.js/index.js
+++ backend_con_express.js/index.js
@@ -2,8 +2,30 @@ const express = require('express')
 const app = express()
 const PORT = 3000;
 
+// Routes
 app.get('/', (req,res) => {
   res.send('Your first server')
+})
+
+app.get('/newRoute', (req,res) => {
+  res.send("Hi, I'm a new route!")
+})
+
+app.get('/products', (req,res) => {
+  res.json([
+    {
+      name: 'ProductOne',
+      price: 10
+    },
+    {
+      name: 'ProductTwo',
+      price: 20
+    },
+    {
+      name: 'ProductThree',
+      price: 30
+    }
+  ])
 })
 
 app.listen(PORT, () => {
