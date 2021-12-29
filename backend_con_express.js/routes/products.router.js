5db346689eaeedd3be385c959681f096a99b2a8f
LaureanoVera <laureanoigvera@gmail.com>
2021-12-29 10:55:45 -0300

    backend con nodejs: introduccion a servicion

--- backend_con_express.js/routes/products.router.js
+++ backend_con_express.js/routes/products.router.js
@@ -1,19 +1,10 @@
 const express = require('express')
-const faker = require('faker')
 const router = express.Router()
+const ProductsService = require('../services/product.service')
+const service = new ProductsService()
 
 router.get('/', (req,res) => {
-  const products = []
-  const { size } = req.query
-  const limit = size || 10
-
-  for (let i = 0; i < limit; i++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: Number(faker.commerce.price()),
-      image: faker.image.sports()
-    })
-  }
+  const products = service.find()
   res.json(products)
 })
 
@@ -23,19 +14,8 @@ router.get('/filter',(req,res) => {
 
 router.get('/:id',(req,res) => {
   const { id } = req.params
-  if (id == '999') {
-    res.status(404).json({
-      id,
-      name: 'ProductThree',
-      price: 30
-    })
-  } else {
-    res.status(200).json({
-      id,
-      name: 'ProductThree',
-      price: 30
-    })
-  }
+  const product = service.findOne(id)
+  res.json(product)
 })
 
 router.post('/', (req,res) => {
