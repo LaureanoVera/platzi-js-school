ae7f101f18cd7ff8071b6c482b405ce34d639258
LaureanoVera <laureanoigvera@gmail.com>
2022-01-22 11:03:53 -0300

    curso de backend con node: instalacion de express y primer server

--- backend_con_node.js/index.js
+++ backend_con_node.js/index.js
@@ -1,1 +1,11 @@
-console.log('Hola');
+const express = require('express');
+const app = express();
+const port = 3019 || process.env.port;
+
+app.get('/', (req, res) => {
+  res.send('Express Server');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
