9da05aa64e7a22660c10460312910c4812a2bc0d
LaureanoVera <laureanoigvera@gmail.com>
2022-01-22 11:18:17 -0300

    curso de backend con node: routing con express.js

--- backend_con_node.js/index.js
+++ backend_con_node.js/index.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send('Express Server');
 });
 
+app.get('/dev', (req, res) => {
+  res.json({
+    name: 'Laureano Vera',
+    contact: 'https://www.linkedin.com/in/laureano-vera-320086204/',
+    age: 19,
+    type: 'warrior',
+  });
+});
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
