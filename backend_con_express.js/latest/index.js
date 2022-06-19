c8d4950a857a9d2fdf3cc2831ff3255482302665
LaureanoVera <laureanoigvera@gmail.com>
2022-06-19 11:34:21 -0300

    backend con express: recibir parametros

--- backend_con_express.js/latest/index.js
+++ backend_con_express.js/latest/index.js
@@ -6,6 +6,38 @@ app.get('/', (req, res) => {
   res.send('First server on express');
 });
 
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'meat',
+      price: 900,
+    },
+    {
+      name: 'rice',
+      price: 80,
+    },
+  ]);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+
+  res.json({
+    name: 'product',
+    price: 999999,
+    id,
+  });
+});
+
+app.get('/categories/:category/products/:id', (req, res) => {
+  const { category, id } = req.params;
+
+  res.json({
+    category,
+    id,
+  });
+});
+
 app.get('/new-route', (req, res) => {
   res.json({
     name: 'route-0111',
